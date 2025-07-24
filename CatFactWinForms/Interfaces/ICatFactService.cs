c6df7135fda748baf288ac5a1807ecea77ced95e
Michuxx <104158118+Michuxx@users.noreply.github.com>
2025-07-24 15:48:03 +0200

    get function from open API and making catFact service and interface

---
+++ CatFactWinForms/Interfaces/ICatFactService.cs
@@ -0,0 +1,14 @@
+﻿using CatFactWinForms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatFactWinForms.Interfaces
+{
+    public interface ICatFactService
+    {
+        Task<CatFactResponse> GetCatFactAsync();
+    }
+}
---
+++ CatFactWinForms/Services/CatFactService.cs
@@ -0,0 +1,33 @@
+﻿using CatFactWinForms.Interfaces;
+using CatFactWinForms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CatFactWinForms.Services
+{
+    public class CatFactService : ICatFactService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CatFactService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+        public async Task<CatFactResponse> GetCatFactAsync()
+        {
+            
+            var response = await _httpClient.GetStringAsync("https://catfact.ninja/fact");
+            var factResponse = JsonSerializer.Deserialize<CatFactResponse>(response);
+            if(factResponse != null)
+            {
+                return factResponse;
+            }
+            
+            throw new Exception("Nie udało się sparsować odpowiedzi z API.");
+        }
+    }
+}
